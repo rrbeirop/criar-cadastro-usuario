e5a64c7dba71ed07dfb07a896c31fafd18dfde8c
Vitor Ribeiro <wmdac24@outlook.com>
2025-01-27 21:26:26 -0400

    2701

--- busca.js
+++ busca.js
@@ -1,3 +1,4 @@
+
 const btn_busca = document.getElementById('btn-busca');
 const busca = document.getElementById('busca');
 const icon_busca = document.getElementById('icon-busca');
@@ -9,3 +10,27 @@ btn_busca.addEventListener('click', () => {
         icon_busca.classList = 'fa-solid fa-search';
     }
 });
+
+//popup.js//
+// const openPopup = document.getElementById('btn-popup'); somente id para abrir o popup
+const fundoPopup = document.getElementById('fundo-popup');
+
+// openPopup.addEventListener('click', () => {
+//     fundoPopup.classList.add('ativo');
+// });
+
+function abrirPopup() {
+    fundoPopup.classList.add('ativo');
+}
+
+function fecharPopup(){
+    fundoPopup.classList.remove('ativo');
+
+}
+fundoPopup.addEventListener('click', (event) => {
+    if (event.target === fundoPopup) {
+        fundoPopup.classList.remove('ativo');
+    }
+});
+
+// fim popup js//
